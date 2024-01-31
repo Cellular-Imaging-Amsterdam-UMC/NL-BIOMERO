c8dd44aec599c58ae1bad882593e38de4fd71d23
Rosas <r.rosas@amsterdamumc.nl>
2024-01-31 12:15:12 +0100

    Working custom login image rotation

--- web/local_omeroweb_edits/get_images_for_login_page.py
+++ web/local_omeroweb_edits/get_images_for_login_page.py
@@ -4,20 +4,18 @@ import re
 
 def update_login_html(image_directory, html_file):
     image_files = [f for f in os.listdir(image_directory) if os.path.isfile(os.path.join(image_directory, f))]
-    # Add indentation to each image tag
-    image_tags = ['        <img src="{{% static \'webclient/image/login_page_images/{}\' %}}" />'.format(f) for f in image_files]
+    # Generate a JavaScript array of image paths
+    image_paths = ['"{{% static \'webclient/image/login_page_images/{}\' %}}"'.format(f) for f in image_files]
+    image_array = 'var images = [{}];'.format(', '.join(image_paths))
 
     with open(html_file, 'r') as file:
         content = file.read()
 
-    # Define start and end placeholders
-    start_placeholder = '<!-- Dynamic Images START -->'
-    end_placeholder = '<!-- Dynamic Images END -->'
+    # Define the placeholder for the JavaScript array
+    placeholder = '// Image Array Placeholder'
 
-    # Use regex to replace content between placeholders
-    pattern = '{}.*{}'.format(re.escape(start_placeholder), re.escape(end_placeholder))
-    replacement = '{}\n{}\n    {}'.format(start_placeholder, '\n'.join(image_tags), end_placeholder)
-    new_content = re.sub(pattern, replacement, content, flags=re.DOTALL)
+    # Replace the placeholder with the JavaScript array
+    new_content = content.replace(placeholder, image_array)
 
     with open(html_file, 'w') as file:
         file.write(new_content)
