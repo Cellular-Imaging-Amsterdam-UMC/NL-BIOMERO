b7649c9aa7392ede34af21f08ec3a456063d6d35
Rosas <r.rosas@amsterdamumc.nl>
2024-01-31 14:38:40 +0100

    tested and cleaned docker-compose and Dockerfile

--- web/local_omeroweb_edits/get_images_for_login_page.py
+++ web/local_omeroweb_edits/get_images_for_login_page.py
@@ -1,8 +1,7 @@
 import os
 import sys
-import re
 
-def update_login_html(image_directory, html_file):
+def update_login_html(image_directory, html_file, destination_file):
     image_files = [f for f in os.listdir(image_directory) if os.path.isfile(os.path.join(image_directory, f))]
     # Generate a JavaScript array of image paths
     image_paths = ['"{{% static \'webclient/image/login_page_images/{}\' %}}"'.format(f) for f in image_files]
@@ -17,11 +16,12 @@ def update_login_html(image_directory, html_file):
     # Replace the placeholder with the JavaScript array
     new_content = content.replace(placeholder, image_array)
 
-    with open(html_file, 'w') as file:
+    with open(destination_file, 'w') as file:
         file.write(new_content)
 
-# Use command line arguments for image directory and HTML file paths
-if len(sys.argv) != 3:
-    print("Usage: python get_images_for_login_page.py <image_directory> <html_file>")
+# Use command line arguments for image directory, HTML file paths, and destination file
+if len(sys.argv) != 4:
+    print("Usage: python get_images_for_login_page.py <image_directory> <html_file> <destination_file>")
 else:
-    update_login_html(sys.argv[1], sys.argv[2])
+    update_login_html(sys.argv[1], sys.argv[2], sys.argv[3])
+
